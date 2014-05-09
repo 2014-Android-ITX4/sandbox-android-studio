2663a59e4a98da8e977178394877a2eb39caee51
Tukasa Kamata <kamatuka3@gmail.com>
2014-05-09 14:32:16 +0900

    MainActivityおよび周辺を修正
    うずらフォントを導入しました

--- app/src/main/java/com/example/sandbox/app/MainActivity.java
+++ app/src/main/java/com/example/sandbox/app/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.sandbox.app;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class MainActivity
@@ -15,6 +24,50 @@ public class MainActivity
   {
     super.onCreate( savedInstanceState );
     setContentView( R.layout.activity_main );
+
+    TextView hello_world = (TextView)findViewById( R.id.hello_world );
+
+    InputStream input_stream = null;
+    BufferedOutputStream output_stream = null;
+    Typeface typeface = null;
+
+    try
+    {
+      // フォントを一時ファイルに
+      String font_file = getCacheDir() + "/tmp" + System.currentTimeMillis() + ".raw";
+
+      input_stream = getResources().openRawResource( R.raw.uzura );
+      output_stream = new BufferedOutputStream( new FileOutputStream( font_file ) );
+
+      byte[] buffer = new byte[ input_stream.available() ];
+      int length = 0;
+
+      while ( ( length = input_stream.read( buffer ) ) > 0 )
+        output_stream.write( buffer, 0, length );
+
+      // 読み込んだら一時ファイルを消す
+      typeface = Typeface.createFromFile( font_file );
+      new File(font_file).delete();
+
+    }
+    catch ( IOException e )
+    { e.printStackTrace(); }
+    finally
+    {
+      if( input_stream != null )
+        try
+        { input_stream.close(); }
+        catch ( IOException e )
+        { e.printStackTrace(); }
+      if( output_stream != null )
+        try
+        { output_stream.close(); }
+        catch ( IOException e )
+        { e.printStackTrace(); }
+    }
+
+    hello_world.setTypeface( typeface );
+
   }
 
 
